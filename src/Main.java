bce154cd92525f361a590538b826a6cce20c713f
alok singh thakur <alokthakur08cse@gmail.com>
2025-03-23 15:37:58 +0530

    running two infinte thread to check how 2 threads run together using implement runnable

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        World world = new World();
+        World world = new World(); //New
         Thread t1 = new Thread(world);
-        t1.start();
+        t1.start(); // Runnable
         for (; ; ) {
             System.out.println("hello");
         }
