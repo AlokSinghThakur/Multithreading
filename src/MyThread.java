5af8e21e4edb19b001c551ff3559ec06666ae660
alok singh thakur <alokthakur08cse@gmail.com>
2025-03-23 23:01:41 +0530

    Thread set prority done

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,23 +1,64 @@
+import java.util.Objects;
+
 public class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
     @Override
     public void run() {
-        System.out.println("RUNNING");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println(e);
+        if (Objects.equals(Thread.currentThread().getName(), "High Priority")) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5000000; j++) {
+
+                }
+                System.out.println(Thread.currentThread().getName() + " -Priority " + Thread.currentThread().getPriority() + " -count " + i);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        if (Objects.equals(Thread.currentThread().getName(), "Medium Priority")) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 1000000; j++) {
+
+                }
+                System.out.println(Thread.currentThread().getName() + " -Priority " + Thread.currentThread().getPriority() + " -count " + i);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        if (Objects.equals(Thread.currentThread().getName(), "Low Priority")) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 500; j++) {
+
+                }
+                System.out.println(Thread.currentThread().getName() + " -Priority " + Thread.currentThread().getPriority() + " -count " + i);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
         }
 
     }
 
+
     public static void main(String[] arg) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        System.out.println(t1.getState());
-        t1.start();
-        System.out.println(t1.getState());
-        Thread.sleep(100);
-        System.out.println(t1.getState());
-        t1.join();
-        System.out.println(t1.getState());
+        MyThread l = new MyThread("Low Priority");
+        MyThread m = new MyThread("Medium Priority");
+        MyThread h = new MyThread("High Priority");
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        h.setPriority(Thread.MAX_PRIORITY);
+        l.start();
+        m.start();
+        h.start();
     }
 }
