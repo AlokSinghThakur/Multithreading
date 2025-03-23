5b4b1a6f663a70c6f15bea35d6c87456918c32bf
alok singh thakur <alokthakur08cse@gmail.com>
2025-03-23 15:48:04 +0530

    Thread Lifecycle

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        World world = new World(); //New
+        World world = new World();
         Thread t1 = new Thread(world);
-        t1.start(); // Runnable
+        t1.start();
         for (; ; ) {
             System.out.println("hello");
         }
