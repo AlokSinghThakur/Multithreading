be96dcebf4d78e2a332dc64ed3a6f41a5de8fd49
alok singh thakur <alokthakur08cse@gmail.com>
2025-04-05 11:52:50 +0530

    Thread Synchronized keyword used

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,24 +1,36 @@
 import java.util.Objects;
 
 public class MyThread extends Thread {
-    public MyThread(String name) {
-        super(name);
+    private Counter  counter;
+    public MyThread(Counter counter){
+        this.counter = counter;
     }
+
 
     @Override
     public void run() {
-        while (true) {
-            System.out.println("Hello world");
+        for (int i=0; i<100000;i++){
+            counter.increment();
         }
     }
-
-
-    public static void main(String[] arg) throws InterruptedException {
-        MyThread t1 = new MyThread("t1");
-        t1.setDaemon(true);
-        t1.start();
-        System.out.println("Main Done");
-    }
+    //    public MyThread(String name) {
+//        super(name);
+//    }
+//
+//    @Override
+//    public void run() {
+//        while (true) {
+//            System.out.println("Hello world");
+//        }
+//    }
+//
+//
+//    public static void main(String[] arg) throws InterruptedException {
+//        MyThread t1 = new MyThread("t1");
+//        t1.setDaemon(true);
+//        t1.start();
+//        System.out.println("Main Done");
+//    }
 }
 
 //  to run on single core process
