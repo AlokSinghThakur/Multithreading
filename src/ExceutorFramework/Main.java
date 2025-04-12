3d72b4583ef21db43a0f02b5709f3f8d4308a6ad
alok singh thakur <alokthakur08cse@gmail.com>
2025-04-13 03:45:24 +0530

    Executor Framework used for factorial

--- src/ExceutorFramework/Main.java
+++ src/ExceutorFramework/Main.java
@@ -1,27 +1,41 @@
 package ExceutorFramework;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        Thread[] threads = new Thread[9];
+        ExecutorService executor = Executors.newFixedThreadPool(9);
+//        Thread[] threads = new Thread[9];
         for (int i = 1; i < 10; i++) {
             int finalI = i;
-            threads[i - 1] = new Thread(
-                    () -> {
-                        long result = factorial(finalI);
-                        System.out.println(result);
-                    }
-            );
-            threads[i - 1].start();
+            executor.submit(() -> {
+                long result = factorial(finalI);
+                System.out.println(result);
+            });
+//            threads[i - 1] = new Thread(
+//                    () -> {
+//                        long result = factorial(finalI);
+//                        System.out.println(result);
+//                    }
+//            );
+//            threads[i - 1].start();
         }
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+//        for (Thread thread : threads) {
+//            try {
+//                thread.join();
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//            }
+//        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(100, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-
         System.out.println("Total time: " + (System.currentTimeMillis() - startTime));
     }
 
